c6259aabce2bde55920221d6b6cd5880d0ef6b20
Hunter Callaway <HunterCallaway@hotmail.com>
2019-06-14 15:59:49 -0600

    Added new components and a background image.

--- angular-todo-list/src/app/app.module.ts
+++ angular-todo-list/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CalculatorComponent } from './calculator/calculator.component';
+import { AboutComponent } from './about/about.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalculatorComponent,
+    AboutComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
