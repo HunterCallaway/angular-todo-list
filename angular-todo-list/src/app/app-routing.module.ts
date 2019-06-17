39823a4bb202c4cdb969da96f896afac3f0735bd
Hunter Callaway <HunterCallaway@hotmail.com>
2019-06-16 19:52:01 -0600

    Added two p tags in about.component.html, structure in navbar.component.html, and imports and routes in app-routing.module.ts.

--- angular-todo-list/src/app/app-routing.module.ts
+++ angular-todo-list/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {CalculatorComponent} from "./calculator/calculator.component";
+import {AboutComponent} from "./about/about.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: CalculatorComponent},
+  {path: 'about', component: AboutComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
