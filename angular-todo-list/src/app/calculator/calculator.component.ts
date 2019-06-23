e47ba0f27365161af6977c3e2980b93c151d91ac
Hunter Callaway <HunterCallaway@hotmail.com>
2019-06-23 16:43:42 -0600

    Installed the Angular version of the Sortable library.

--- angular-todo-list/src/app/calculator/calculator.component.ts
+++ angular-todo-list/src/app/calculator/calculator.component.ts
@@ -45,6 +45,11 @@ export class CalculatorComponent implements OnInit {
 			woohoo.play();
 		};
 
+//This function utilizes the Angular version
+// of the Sortable library (https://www.npmjs.com/package/angular-sortablejs)
+//to allow the user to re-arrange the list items in order of priority.
+
+
 //Build a to-do element and return it.
 		const buildTodoEl = (todo) => {
 			//Create a list item element.
